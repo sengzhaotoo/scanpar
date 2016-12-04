450b1916479420f287f1a1a14b4c480ec8b475ce
Jessie Nelson <jnelso23@hawk.iit.edu>
2016-12-04 12:49:03 -0600

    Updating parser to recognize array. Removing redundant backup files

--- parser.c
+++ parser.c
@@ -16,19 +16,12 @@ typedef enum{
 } assignment;
 
 typedef enum{
-  plus,
-  minus,
-  divided_by,
-  times
-} operator;
-
-typedef enum{
   Int, 
   Float
 } type;
 
 struct identifier{
-  string name;
+  string name;  
   type t;
 };
 
@@ -38,39 +31,48 @@ struct term{
 };
 
 struct operation{
-  operator op;
+  string op;
   struct term *term1;
   struct term *term2;
 };
 
-struct equality{
+struct expression{
   assignment eq;
   struct identifier *id;
   struct operation *op;
 };
 
 // Function Prototypes
+int isArray(string *arr);
 int isAssignment(string *arr);
-
 int isExpression(string *arr);
-
 int isOp(string operator);
 
-struct equality* newEquality(string *arr);
+struct expression* newExpression(string *arr);
+struct identifier* newIdentifier(string *arr);
+struct operation* newOperation(string *arr);
 
 // Main Function
 int main(int argc, string argv[]){
   
   
   string array[6] = {"a", "=", "1", "+", "2", ";"}; // Example input 
-  newEquality(array);
+  if (isAssignment(array)){
+    if (isExpression(array)){
+      newExpression(array);
+    }
+  }
 
 }
 
 //Function definitions
 
+int isArray(char **arr){
+  return arr[1] == "[";
+}
+
 int isAssignment(char **arr){
-  return arr[1] == "=";
+  return arr[1] == "=" || arr[4] == "=";
 }
 
 int isExpression(char **arr){
@@ -81,9 +83,33 @@ int isOp(string operator){
   return operator == "+" || operator == "-" || operator == "*" || operator == "/";
 }
 
-struct equality* newEquality(string *arr){
-  struct equality* eq_node = malloc(sizeof(struct equality)); 
+struct expression* newExpression(string *arr){
+  struct expression* eq_node = malloc(sizeof(struct expression)); 
   eq_node->eq = equals; 
- // struct identifier i;
- // struct operation p;
+  struct identifier* i = newIdentifier(arr);
+  struct operation* p = newOperation(arr);
 }
+
+struct identifier* newIdentifier(string *arr){
+  struct identifier* id_node = malloc(sizeof (struct identifier));
+  if (!isArray(arr)){   
+  
+  }
+  else{  
+  
+  } 
+  id_node->name = arr[0];
+  id_node->t;
+}
+
+struct operation* newOperation(string *arr){
+  struct operation* op_node = malloc(sizeof(struct operation));
+  struct term* term_node1 = malloc(sizeof(struct term));
+  struct term* term_node2 = malloc(sizeof(struct term));
+  op_node->op = arr[3];
+  op_node->term1 = term_node1;
+  op_node->term2 = term_node2;
+  term_node1->literal = arr[2];
+  term_node2->literal = arr[4];
+  op_node->term1 = term_node1;  
+}  
